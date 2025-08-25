480c0843bd8a21f2b6030a44bc18b9355a36efaa
gajendrajangid83 <saraswatisharmapbc@gmail.com>
2025-08-25 13:40:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,34 +4,34 @@ from os import getenv
 
 
 # ------------------------------------------------
-API_ID = int(os.environ.get("API_ID", "20114039"))
+API_ID = int(os.environ.get("API_ID", "21157244"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH","87297b8f3cc8fc9bbce591ad30da5896")
+API_HASH = os.environ.get("API_HASH","4981c2699bd91c7db836ec8f77e5b0f0")
 # ------------------------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7690648371:AAFFDqm-tIOMymaZ-5YSVWNQe8lQpZkLGLg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Bhumihar_PWEXT_bot")
-BOT_TEXT = "❖────[『 WELCOME BHUMIHAR 』]────❖"
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@extractorbygajju_bot")
+BOT_TEXT = "❖────[『 WELCOME GAJENDRA 』]────❖"
 # ------------------------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "8172163893"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1783306092"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002614908937"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002329582972"))
 
 # //FORCE_CHANNEL_ID
-CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002500606405")) 
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002329582972")) 
 # -----------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://Demo23:Demo23@cluster0.fjar36c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002511678727"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002329582972"))
 # -----------------------------------------------
-join = '<a href="https://t.me/BHUMIHARBOTSS">✳️ BᴀᴄKUP</a>'
+join = '<a href="https://t.me/RPSC_UPSC_CURRENT">✳️ BᴀᴄKUP</a>'
 # -----------------------------------------------
 UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "Thebhumihar" #without
+ADMIN_BOT_USERNAME = "Mirza_Ghallib" #without
 
 THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/ZRvCxd5g/STRANGER-BOY.jpg")
 
@@ -39,14 +39,14 @@ THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/ZRvCxd5g/STRANGER-BOY.
 
 
 # # Bot configuration
-# API_ID = int(os.environ.get("API_ID", "22746239"))
-# API_HASH = os.environ.get("API_HASH", "a98ec8cfd8572a3a7c936cf828fe6215")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN", "7547829346:AAGyfvOu47EciNhC7NUGSDEDFuBaetYYusw")
-# BOT_USERNAME = os.environ.get("BOT_USERNAME", "MassRPBot")
-# OWNER_ID = int(os.environ.get("OWNER_ID", "7463601722"))
-# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7463601722").split()))
-# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002601604234"))
+# API_ID = int(os.environ.get("API_ID", "21157244"))
+# API_HASH = os.environ.get("API_HASH", "4981c2699bd91c7db836ec8f77e5b0f0")
+# BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+# BOT_USERNAME = os.environ.get("BOT_USERNAME", "@extractorbygajju_bot")
+# OWNER_ID = int(os.environ.get("OWNER_ID", "1783306092"))
+# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1783306092").split()))
+# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 # MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002601604234"))
+# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002329582972"))
 # THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
 
